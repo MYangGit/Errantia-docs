22dad148c08aa62f43cf1d75895d25e40c05f587
MYangGit <93202929+MYangGit@users.noreply.github.com>
2024-03-05 16:53:20 +0800

    chore：初始化搭建组件库文档站点

---
+++ blog/2024-03-05-welcome/index.md
@@ -0,0 +1,10 @@
+---
+slug: welcome
+title: Welcome
+authors: [MY]
+tags: [tongyuan, hello, Errantia]
+---
+## 你好 Errantia
+![Docusaurus Plushie](./docusaurus-plushie-banner.jpeg)
+
+
---
+++ docusaurus.config.js
@@ -0,0 +1,139 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Errantia 文档',
+  tagline: '深圳-景元，跨端app组件化解决方案。',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Errantia',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'articleSidebar',
+            position: 'left',
+            label: '文档',
+          },
+          {
+            type: 'docSidebar',
+            sidebarId: 'moduleSidebar',
+            position: 'left',
+            label: '组件库',
+          },
+          {to: '/blog', label: '日志', position: 'right'},
+          {
+            href: 'https://github.com/MYangGit/Errantia-docs',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: '文档',
+                to: '/docs/article/quick_start/installUse',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: '同元软控',
+                href: 'https://www.tongyuan.cc',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: '日志',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/MYangGit/Errantia-docs',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Errantia, Inc. Built with Tongyuan.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
